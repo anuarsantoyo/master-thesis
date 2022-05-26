edadc85f81f081f0db7cc8567fa6886245712775
Timo9092 <t.rohlmann96@gmail.com>
2022-05-26 10:55:20 +0000

    model changes

--- model/modelhelper.py
+++ model/modelhelper.py
@@ -14,7 +14,7 @@ import numpy as np
 
 # Details Model Parameter
 dict_model_param = {'lower': {'R0': 0.001, 'phi': 0, 'sigma': 0.001, 'alpha': 0.001},
-                    'upper': {'R0': 3, 'phi': 50, 'sigma': 0.5, 'alpha': 0.056},
+                    'upper': {'R0': 3, 'phi': 50, 'sigma': 0.5, 'alpha': 0.05},
                     'value': {'R0': 1.5, 'phi': 25, 'sigma': 0.1, 'alpha': 0.028},
                     'scale': {'R0': 1, 'phi': 10, 'sigma': 0.02, 'alpha': 0.002}}
 
